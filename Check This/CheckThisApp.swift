d395be16b907dc440bb30c150dd8a0ef78ee52d9
ostojan <a.stojanowski@outlook.com>
2022-08-15 18:10:29 +0200

    Initial Commit

---
+++ Check This/CheckThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_ThisApp.swift
+//  Check This
+//
+//  Created by ostojan on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
