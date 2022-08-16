859c2ab2aae09c0f4b4acee30e67cd367f532b09
ostojan <a.stojanowski@outlook.com>
2022-08-16 21:10:38 +0200

    ModelData class with mock data

---
+++ Check This/Models/ModelData.swift
@@ -0,0 +1,19 @@
+//
+//  ModelData.swift
+//  Check This
+//
+//  Created by ostojan on 16/08/2022.
+//
+
+import Foundation
+
+final class ModelData: ObservableObject {
+    @Published var items = [
+        Item(name: "Task 1"),
+        Item(name: "Task 2", done: true),
+        Item(name: "Task 3"),
+        Item(name: "Task 4", done: true),
+        Item(name: "Task 5"),
+        Item(name: "Task 6", done: true),
+    ]
+}
