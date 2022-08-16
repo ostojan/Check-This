418e28f26dcf8568711d83521d2f7b69a4b4adbf
ostojan <a.stojanowski@outlook.com>
2022-08-16 21:32:02 +0200

    Pass ModelData instance into app

--- Check This/CheckThisApp.swift
+++ Check This/CheckThisApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct CheckThisApp: App {
+    private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
