8bdbb1c2c91009b417f8c9c09ec7fe27dc21c99f
ostojan <a.stojanowski@outlook.com>
2022-11-14 20:38:37 +0100

    ItemManager class

---
+++ Check This/Managers/ItemManager.swift
@@ -0,0 +1,43 @@
+//
+//  ItemManager.swift
+//  Check This
+//
+//  Created by ostojan on 14/11/2022.
+//
+
+import Foundation
+
+class ItemManager {
+    private let viewContext = PersistenceController.shared.viewContext
+    
+    func fetchAllItems() -> [Item] {
+        let request = Item.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "created", ascending: true)]
+        do {
+            return try viewContext.fetch(request)
+        } catch {
+            print("Couldn't fetch items: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func createNewItem(name: String, done: Bool = false, created: Date = Date()) {
+        let newItem = Item(context: viewContext)
+        newItem.id = UUID()
+        newItem.name = name
+        newItem.done = done
+        newItem.created = created
+    }
+    
+    func delete(_ item: Item) {
+        viewContext.delete(item)
+    }
+
+    func saveData() {
+        do {
+            try viewContext.save()
+        } catch {
+            print("Couldn't save data: \(error.localizedDescription)")
+        }
+    }
+}
